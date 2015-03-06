802d3856838f0463546f83ad3391eeb34499e28b
paule96 <paul.jeschke@outlook.com>
2015-03-06 12:40:52 +0100

    Custom Marker für Parkplatz hinzugefügt Legende muss noch hinzugefügt werden!

--- js/map.js
+++ js/map.js
@@ -7,16 +7,17 @@ function initialize() {
 
     var map = new google.maps.Map(document.getElementById('map-div'),
                       mapOptions);
-    var marker = new google.maps.Marker({
+    new google.maps.Marker({
         position: new google.maps.LatLng(51.068858, 13.716060),
         map: map,
         icon: iconBase + 'red-circle.png'
+
     });
     iconBase = "https://maps.google.com/mapfiles/kml/shapes";
-    var markerp = new google.maps.Marker({
+    new google.maps.Marker({
         position: new google.maps.LatLng(51.067566, 13.719582),
         map: map,
-        icon: iconBase + 'parking_lot.png'
+        icon: "http://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png"
     });
 
 }
