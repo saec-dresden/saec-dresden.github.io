82e14a9064d57b593660c13af802858f771ff2a3
paule96 <paul.jeschke@outlook.com>
2015-03-11 08:18:11 +0100

    Google Maps Legende hinzugefügt

--- js/map.js
+++ js/map.js
@@ -3,6 +3,14 @@ function initialize() {
         zoom: 16,
         center: new google.maps.LatLng(51.068858, 13.716050)
     };
+
+    //ICONS
+    var icondevday = "http://maps.google.com/mapfiles/kml/paddle/red-circle.png";
+    var iconpark = "http://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png";
+
+
+
+    //Der Marker für die Messe
     var iconBase = "http://maps.google.com/mapfiles/kml/paddle/";
 
     var map = new google.maps.Map(document.getElementById('map-div'),
@@ -10,15 +18,29 @@ function initialize() {
     new google.maps.Marker({
         position: new google.maps.LatLng(51.068858, 13.716060),
         map: map,
-        icon: iconBase + 'red-circle.png'
+        icon: icondevday,
+        title: "Dev Day 2015 \nDresden Messering 6"
 
     });
-    iconBase = "https://maps.google.com/mapfiles/kml/shapes";
+
+    //der Marker für den Parkplatz
     new google.maps.Marker({
         position: new google.maps.LatLng(51.067566, 13.719582),
         map: map,
-        icon: "http://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png"
+        icon: iconpark
     });
+
+    //Legende
+    var legende = $("<div id='legende' style='background:#FFFFFF; position:absolute'></div>");
+
+    var divs = [document.createElement('div'), document.createElement('div')];
+    divs[0].innerHTML = "<img width='20em' height='20em' src='"+icondevday+"'> Dev Day";
+    divs[1].innerHTML = "<img width='20em' height='20em' src='"+iconpark+"'> Parkplatz";
+    legende[0].appendChild(divs[0]);
+    legende[0].appendChild(divs[1]);
+
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legende[0]);
+
 
 }
 
